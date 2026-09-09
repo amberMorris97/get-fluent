289a34855938e4626dead504b3636ee44d4c7136
Amber Morris <ambermorris1997@gmail.com>
2026-09-09 15:39:21 -0400

    implement auth context

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useState } from "react";
+import { getEmailFromStorage, getTokenFromStorage, removeTokenFromStorage } from "../services/storageService";
+import { validateToken } from "../services/authService";
+
+const initialAuth = {
+    token: getTokenFromStorage() || null,
+    email: getEmailFromStorage() || null,
+    isAuthenticated: false,
+};
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+    const [ auth, setAuth ] = useState(initialAuth);
+
+    const checkAuthStatus = async () => {
+        if (auth.token && auth.email) {
+            try {
+                await validateToken({ token: auth.token, email: auth.email });
+                setAuth({
+                    token: auth.token,
+                    email: auth.email,
+                    isAuthenticated: true,
+                });
+            } catch (error) {
+                removeTokenFromStorage();
+                setAuth(initialAuth);
+            }
+        }
+    };
+
+    useEffect(() => {
+        checkAuthStatus();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{auth, setAuth}}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
