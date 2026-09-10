fac7a0684437ca5513158dbd5af9011a8e934778
Amber Morris <ambermorris1997@gmail.com>
2026-09-10 14:43:29 -0400

    wrap app in auth context provider

--- src/components/pages/auth/RegisterPage.jsx
+++ src/components/pages/auth/RegisterPage.jsx
@@ -1,7 +1,17 @@
+import { requestLogin } from "../../../services/authService";
 import FormWrapper from "./FormWrapper";
 import RegisterForm from "./RegisterForm";
 
 const RegisterPage = () => {
+
+    const registerAndLogInUser = async () => {
+        try {
+            let response = await requestLogin({ email, password });
+            // if (response?.status === 200)
+        } catch(error) {
+            
+        }
+    }
   const subtitle = "New to Get Fluent Creole? Register here!";
     return (
         <div className="register-page">
