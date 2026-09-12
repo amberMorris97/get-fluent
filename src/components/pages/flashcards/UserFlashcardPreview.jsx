2d8420e6bb7f8aa771b7f0cde449cc61a9b940b9
Amber Morris <ambermorris1997@gmail.com>
2026-09-12 14:45:27 -0400

    add content to user flashcard preview

--- src/components/pages/flashcards/UserFlashcardPreview.jsx
+++ src/components/pages/flashcards/UserFlashcardPreview.jsx
@@ -1,12 +1,17 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const UserFlashcardPreview = ({ flashcard }) => {
     console.log(flashcard)
     return (
         <div className="user-flashcard-preview">
-            <div>{flashcard.phrase.haitianCreole}</div>
-            <div>{flashcard.phrase.english}</div>
-            <div>{flashcard.phrase.pronunciation}</div>
-            <div>
+            <FontAwesomeIcon icon="fa-solid fa-trash" />
+            <h2>{flashcard.phrase.haitianCreole}</h2>
+            <h3>{flashcard.phrase.english}</h3>
+            <h5 className="phrase-pronunciation">{flashcard.phrase.pronunciation}</h5>
+            <div className="flashcard-status-box">
+                <FontAwesomeIcon icon="fa-solid fa-circle-check" />
                 <h6>Mastered</h6>
+                <FontAwesomeIcon icon="fa solid fa-circle-xmark" />
                 <h6>Needs Work</h6>
             </div>
         </div>
